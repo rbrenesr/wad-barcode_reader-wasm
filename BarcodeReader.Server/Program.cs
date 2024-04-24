4fdfa0c0e025d1f10dad9f3bc6623a8cac816864
Rafael Brenes <rbrenesr@gmail.com>
2024-04-24 14:42:04 -0600

    feat(init Sln) Proyectos iniciales, estrucura base blazor wasm hosted {rb}
     Changes to be committed:
    	new file:   BarcodeReader.Client/App.razor
    	new file:   BarcodeReader.Client/BarcodeReader.Client.csproj
    	new file:   BarcodeReader.Client/Layout/MainLayout.razor
    	new file:   BarcodeReader.Client/Layout/MainLayout.razor.css
    	new file:   BarcodeReader.Client/Layout/NavMenu.razor
    	new file:   BarcodeReader.Client/Layout/NavMenu.razor.css
    	new file:   BarcodeReader.Client/Pages/Counter.razor
    	new file:   BarcodeReader.Client/Pages/Home.razor
    	new file:   BarcodeReader.Client/Pages/Weather.razor
    	new file:   BarcodeReader.Client/Program.cs
    	new file:   BarcodeReader.Client/Properties/launchSettings.json
    	new file:   BarcodeReader.Client/_Imports.razor
    	new file:   BarcodeReader.Client/wwwroot/css/app.css
    	new file:   BarcodeReader.Client/wwwroot/css/bootstrap/bootstrap.min.css
    	new file:   BarcodeReader.Client/wwwroot/css/bootstrap/bootstrap.min.css.map
    	new file:   BarcodeReader.Client/wwwroot/favicon.png
    	new file:   BarcodeReader.Client/wwwroot/icon-192.png
    	new file:   BarcodeReader.Client/wwwroot/icon-512.png
    	new file:   BarcodeReader.Client/wwwroot/index.html
    	new file:   BarcodeReader.Client/wwwroot/manifest.webmanifest
    	new file:   BarcodeReader.Client/wwwroot/sample-data/weather.json
    	new file:   BarcodeReader.Client/wwwroot/service-worker.js
    	new file:   BarcodeReader.Client/wwwroot/service-worker.published.js
    	new file:   BarcodeReader.Server/BarcodeReader.Server.csproj
    	new file:   BarcodeReader.Server/BarcodeReader.Server.http
    	new file:   BarcodeReader.Server/Controllers/WeatherForecastController.cs
    	new file:   BarcodeReader.Server/Program.cs
    	new file:   BarcodeReader.Server/Properties/launchSettings.json
    	new file:   BarcodeReader.Server/WeatherForecast.cs
    	new file:   BarcodeReader.Server/appsettings.Development.json
    	new file:   BarcodeReader.Server/appsettings.json
    	new file:   BarcodeReader.Shared/BarcodeReader.Shared.csproj
    	new file:   BarcodeReader.Shared/WeatherForecast.cs
    	new file:   BarcodeReader.sln

---
+++ BarcodeReader.Server/Program.cs
@@ -0,0 +1,36 @@
+using System.Text.Json.Serialization;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews()
+    .AddJsonOptions(option =>
+        option.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+
+builder.Services.AddRazorPages();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    app.UseWebAssemblyDebugging();
+}
+
+app.UseHttpsRedirection();
+
+app.UseBlazorFrameworkFiles();
+app.UseStaticFiles();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+app.MapControllers();
+app.MapFallbackToFile("index.html");
+
+app.Run();
